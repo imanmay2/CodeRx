33a7720d736bffc51dcf9e0a78b28f21b39d6389
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2025-07-03 22:18:22 +0530

    added logout route

--- server/app.cjs
+++ server/app.cjs
@@ -11,11 +11,11 @@ const cookieParser = require("cookie-parser");
 
 
 //initialize
-const Admin=require("./models/Admin.cjs");
-const Club=require("./models/clubs.cjs");
-const Faculty=require("./models/faculty.cjs");
-const Student=require("./models/students.cjs");
-const User=require("./models/User.cjs");
+const Admin = require("./models/Admin.cjs");
+const Club = require("./models/clubs.cjs");
+const Faculty = require("./models/faculty.cjs");
+const Student = require("./models/students.cjs");
+const User = require("./models/User.cjs");
 
 const corsOptions = {
     origin: "http://localhost:5173",
@@ -53,16 +53,16 @@ app.listen(PORT, (req, res) => {
 });
 
 
-app.get("/getData",(req,res)=>{
+app.get("/getData", (req, res) => {
     console.log("Database hitted");
-    res.json({message:"Hello From Backend !! "});
+    res.json({ message: "Hello From Backend !! " });
 });
 
 
 // SignUp Route.
 app.post("/signUp", async (req, res) => {
-    const { Name, Email, Password,Role,id } = req.body;
-    let userRes = await User.find({ email:Email,id:id});
+    const { Name, Email, Password, Role, id } = req.body;
+    let userRes = await User.find({ email: Email, id: id });
     if (!userRes.length) {
         try {
             console.log(req.body);
@@ -71,26 +71,36 @@ app.post("/signUp", async (req, res) => {
             console.log(emailValidation);
             if (emailValidation == true) {
                 hashPass = await bcrypt.hash(Password, saltRounds);   //Encryption of the password.
-                    /// yet to be done
+                /// yet to be done
                 const user1 = new User({
                     name: Name,
                     email: Email,
                     password: hashPass,
-                    role:Role,
-                    id:id //either regNo or faculty_id or Admin_id
+                    role: Role,
+                    id: id //either regNo or faculty_id or Admin_id
                 });
                 await user1.save();
-                if(Role==="faculty"){
-                    const faculty1=new Faculty({
-                    userId:user1._id
-                });
-                faculty1.save();
+
+                //cookies
+                res.cookie("login", "true", { secure: false });
+                res.cookie("id", id, { secure: false });
+                // res.cookie("name", Name, { secure: false });  // secure false as using http. not https.
+                if (Role === "faculty") {
+                    const faculty1 = new Faculty({
+                        userId: user1._id
+                    });
+                    faculty1.save();
                 }
-                 else if(Role==="president"){
-                    const student1=new Student({
-                    userId:user1._id
-                });
-                student1.save();
+                else if (Role === "president") {
+                    const student1 = new Student({
+                        userId: user1._id
+                    });
+                    student1.save();
+                } else if (Role === "admin") {
+                    const admin1 = new Admin({
+                        userId: user1._id
+                    })
+                    admin1.save();
                 }
                 console.log("User Signed Up!!");
                 flag = 1;
@@ -103,7 +113,7 @@ app.post("/signUp", async (req, res) => {
             res.status(500).json({ 'message': "Error in pushing the data. ", "flag": "error" });
         }
     } else {
-        res.json({ 'message': 'User already exists ! ',flag:"error" });
+        res.json({ 'message': 'User already exists ! ', flag: "error" });
     }
 });
 
@@ -113,60 +123,83 @@ app.post("/signUp", async (req, res) => {
 app.post("/login", async (req, res) => {
     // let flag = 0;
     const { Email, Password } = req.body;
-    let userRes = await User.find({ email:Email});
-    if (userRes.length){
+    let userRes = await User.find({ email: Email });
+    if (userRes.length) {
         let hashPass = userRes[0].password;
         bcrypt.compare(Password, hashPass, function (err, result) {
             if (result) {
                 flag = 1;
-                res.json({ "message": "User Logged in Successfully", "flag": "success" ,role:userRes[0].role});
+                res.cookie("login", "true", { secure: false });
+                res.cookie("id", userRes[0].id, { secure: false });
+                res.json({ "message": "User Logged in Successfully", "flag": "success", role: userRes[0].role });
             } else {
                 res.json({ "message": "Password is incorrect ! ", "flag": "error" });
             }
         });
     } else {
-        res.json({ "message": "Email is incorrect ! ", "flag": "error"});
+        res.json({ "message": "Email is incorrect ! ", "flag": "error" });
     }
 });
 
 
 
 //creating a new Club 
-app.post("/createNewClub",async(req,res)=>{
+app.post("/createNewClub", async (req, res) => {
     //  name: '', president: '', facultyCoordinator: '', maxMemberCount: '',
     //     category: '', status: 'Active'
-    let {name,president,facultyCoordinator,maxMemberCount,category,status}=req.body;
+    let { name, president, facultyCoordinator, maxMemberCount, category, status } = req.body;
     console.log(req.body);
-    try{
-        let clubDetails=await Club.find({clubName:name});
-        let facultyDetails=await Faculty.find({faculty_id:facultyCoordinator}).populate('userId');
-        let studentDetails=await Student.find({regNo:president}).populate('userId');
-        console.log("faculty_details_: "+facultyDetails);
-        console.log("Student _deatils:  "+studentDetails);
+    try {
+        let clubDetails = await Club.find({ clubName: name });
+        let facultyDetails = await User.find({ id: facultyCoordinator, role: "faculty" });
+        let studentDetails = await User.find({ id: president, role: "president" });
+        console.log("faculty_details_: " + facultyDetails);
+        console.log("Student _detils:  " + studentDetails);
         //also check for the faculty and president already present in the database or not.
-        if(!clubDetails.length){
-            if(facultyDetails.length){
-                if(studentDetails.length){
-                    let newClub_=new Club({
-                        clubName:name,
-                        clubPresident:studentDetails[0].userId.id,
-                        category:category,
-                        faculty:facultyDetails[0].userId.id,
-                        status:status,
-                        maxMembers:maxMemberCount
+        if (!clubDetails.length) {
+            if (facultyDetails.length) {
+                if (studentDetails.length) {
+                    let newClub_ = new Club({
+                        clubName: name,
+                        clubPresident: studentDetails[0].name,
+                        category: category,
+                        faculty: facultyDetails[0].name,
+                        status: status,
+                        maxMembers: maxMemberCount,
+
                     });
                     newClub_.save();
-                    res.json({message:"Club Created Successfully",flag:"success"});
-                } else{
-                    res.json({message:"President Not Found",flag:"error"});
+                    res.json({ message: "Club Created Successfully", flag: "success" });
+                } else {
+                    res.json({ message: "President Not Found", flag: "error" });
                 }
-            }else{
-                res.json({message:"Faculty Not Found",flag:"error"});
+            } else {
+                res.json({ message: "Faculty Not Found", flag: "error" });
             }
-        } else{
-            res.json({message:"Club Already Exists",flag:"error"});
+        } else {
+            res.json({ message: "Club Already Exists", flag: "error" });
         }
-    } catch(err){
-        res.json({message:err.message,flag:"error"});
+    } catch (err) {
+        res.json({ message: err.message, flag: "error" });
     }
+});
+
+
+
+//get club Data.
+app.get("/getClubData", async (req, res) => {
+    try {
+        const clubData = await Club.find();
+        res.json(clubData);
+    } catch (err) {
+        res.json({ message: err.msg, flag: "error" });
+    }
+})
+
+
+
+//logging out.
+app.get("/logout", async (req, res) => {
+    res.cookie("login", "false", { secure: false });
+    res.json({ message: "Logged Out Sucessfully", "flag": "success" });
 });
